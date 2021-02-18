a0d1e1cc1d8bd9c3c8985b87b8d4999e7bef3bc2
Nidhi Prajapati <nidhi.prajapati@greylabs.com>
2021-02-18 22:53:56 +0530

    first commit!

--- Demo.js
+++ Demo.js
@@ -2,10 +2,9 @@
 // const git = simpleGit();
 
 require('simple-git')()
-     .init()
-     .add('./*')
-     .commit("first commit!")
-     .addRemote('origin', 'https://github.com/NidhiPrajapati1998/repo.git')
-    //  .push('origin', 'master');
+    .init()
+    .add('./*')
+    .commit("first commit!")
+    .addRemote('origin', 'https://github.com/NidhiPrajapati1998/repo.git')
     .push(['-u', 'origin', 'master'], () => console.log('done'));
-     console.log("hello");
+console.log("hello");
